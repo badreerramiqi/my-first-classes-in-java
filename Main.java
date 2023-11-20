e271adc0be1aa172acc10461aea47c3a778c5598
Badre ER-Ramiqi <147147111+badreerramiqi@users.noreply.github.com>
2023-11-20 21:44:56 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main (String[] args) {
+        Employer emp1 = new Employer() ;
+        emp1.number = 1 ;
+        emp1.name = "Badre" ;
+        emp1.sales = 3000 ;
+        emp1.bonushrs = 150 ;
+        emp1.basesalary = 20000 ;
+
+        emp1.info();
+    }
+}
